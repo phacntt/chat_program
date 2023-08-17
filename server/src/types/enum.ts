e96e16c6f353dd315759d6a1f4ee6620de73eecb
phacntt <trananhpha2001@gmail.com>
2023-08-18 01:28:35 +0700

    initialization type to handle message from client

---
+++ server/src/types/enum.ts
@@ -0,0 +1,12 @@
+export enum TypeOfMessage {
+    Text = "text",
+    File = "file"
+};
+
+export enum ActionOfMessage {
+    CreateRoom = "create-room",
+    JoinRoom = "join-room",
+    ListRooms = "list-rooms",
+    SendMessage = "send-message",
+    ListMessages = "list-messages"
+};
---
+++ server/src/types/messageAction.type.ts
@@ -0,0 +1,37 @@
+import { ActionOfMessage } from "./enum"
+
+export type MessageAction = {
+    action: ActionOfMessage,
+    data: MessageChat | MessageCreateRoom | MessageJoinRoom | MessageListRoomsByAuthor | MessageListMessagesByRoomId
+}
+
+export type MessageReturn = {
+    action: ActionOfMessage,
+    data: any
+}
+
+export type MessageChat = {
+    roomId: string,
+    content: string,
+    author: string,
+    time: string,
+}
+
+export type MessageCreateRoom = {
+    roomName: string,
+    author: string,
+}
+
+export type MessageJoinRoom = {
+    roomId: string,
+    author: string,
+}
+
+
+export type MessageListRoomsByAuthor = {
+    author: string,
+}
+
+export type MessageListMessagesByRoomId = {
+    roomId: string,
+}
