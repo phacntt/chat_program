c75a609a21e936944f8d138401f5e1be33633576
phacntt <trananhpha2001@gmail.com>
2023-08-19 00:29:25 +0700

    use username local to set page start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,23 @@ import React, { useState } from "react";
 import ContainerLayout from "components/layouts/ContainerLayout";
 import LoginLayout from "components/layouts/LoginLayout";
 import "index.css";
+import { VariableLocal } from "constant";
 
 const App: React.FC = () => {
   const [userName, setUserName] = useState<string | null>("");
   
   const handleUsername = (username: string | null) => {
     setUserName(username);
-    localStorage.setItem("username", username!);
+    localStorage.setItem(VariableLocal.username, username!);
   };
 
+  const checkUserLogin = localStorage.getItem(VariableLocal.username);
 
-  return !userName ? (
+
+  return !checkUserLogin ? (
     <LoginLayout handleUsername={handleUsername} />
   ) : (
-    <ContainerLayout username={userName}/>
+    <ContainerLayout username={checkUserLogin}/>
   );
 };
 
